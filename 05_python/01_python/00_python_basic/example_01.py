e3e0c52bfc54e62c8b0c92670a474d36ac367729
wangyu <kimwg02@naver.com>
2019-12-12 11:47:02 +0900

    added dicts

---
+++ 05_python/01_python/00_python_basic/example_01.py
@@ -0,0 +1,25 @@
+import random
+
+# greeting = '안녕하세요.'
+
+# for i in range(5):
+#     print(greeting)
+
+# n = 0
+# while n < 5:
+#     print(greeting)
+#     n += 1
+
+# tiny_dust = 130
+# if tiny_dust >= 150:
+#     print("매우나쁨")
+# elif tiny_dust > 80, 150 <= tiny_dust:
+#     print("나쁨")
+# elif tiny_dust > 30, 80 <= tiny_dust:
+#     print("보통")
+# else:
+#     print("정상")
+
+
+numbers = random.sample(range(1,46), 6)
+print(numbers)
